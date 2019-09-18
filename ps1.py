bb7eaea095ffc86fb0ebff92262091cd0a8375fc
anirudh <jtm192672@gmail.com>
2019-09-18 10:28:46 +0530

    first hour work

--- ps1.py
+++ ps1.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3.6
+bi=input("Enter binary bit data that has to be transmitted:")
+lis=list(bi)
+one=lis.count("1")
+if one%2==0:
+	print("Parity bit data:"+bi+"1")
+
+
+
+
+
+
+
+
+
 
